616191f73ed3dbdefaa741dff457c527fa050e91
MohanSai-S <sathyanarayanamohansai.gmail.com>
2021-04-29 19:11:06 +0530

    Added : Program shows Intial position of a player

--- src/snakeandladdergame/SnakeAndLadder.java
+++ src/snakeandladdergame/SnakeAndLadder.java
@@ -1,5 +1,12 @@
 package snakeandladdergame;
 
 public class SnakeAndLadder {
-
+	//Constants
+	public static final int player1 = 0; 
+	
+	public static void main(String[] args) {
+		
+		int posOfPlayer1=0;
+		System.out.println("Player is At the initial position,"+ 0);
+	}
 }
